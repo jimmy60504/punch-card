54b17b62078a909fe8fc14d0c4d0706893466442
jimmy60504 <jimmy60504@gmail.com>
2019-05-04 00:25:48 +0800

    add chrome

--- punch.py
+++ punch.py
@@ -1,31 +1,36 @@
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException, StaleElementReferenceException
 
-studentID = "00000"
-studentPW = "*****"
+student_id = "11111"
+student_pw = "*****"
 work_name = "工讀：地科系-勞僱型教學助理"
 work_info = "協助教學"
 
+# driver = webdriver.Chrome()
 driver = webdriver.Firefox()
-driver.get("https://cis.ncu.edu.tw/HumanSys/")
+
 
 # HumanSys login
+driver.get("https://cis.ncu.edu.tw/HumanSys/")
 login_button = driver.find_element_by_xpath("/html/body/header/nav/ul/li/a")
 login_button.click()
 
+
 # Portal login
-account = driver.find_element_by_xpath('//*[@id="userid_input"]')
-password = driver.find_element_by_xpath("/html/body/div/div[2]/div[1]/div/div/form/fieldset/div[2]/div/input")
+account_box = driver.find_element_by_xpath('//*[@id="userid_input"]')
+password_box = driver.find_element_by_xpath("/html/body/div/div[2]/div[1]/div/div/form/fieldset/div[2]/div/input")
 submit_button = driver.find_element_by_xpath("/html/body/div/div[2]/div[1]/div/div/form/fieldset/div[3]/div/button")
 
-account.send_keys(studentID)
-password.send_keys(studentPW)
+account_box.send_keys(student_id)
+password_box.send_keys(student_pw)
 submit_button.click()
+
 
 # HumanSys home
 driver.get("https://cis.ncu.edu.tw/HumanSys/student/stdSignIn")
 
-# HumanSys sign in
+
+# HumanSys signin table
 table = driver.find_element_by_xpath('//*[@id="table1"]')
 rows = table.find_elements_by_tag_name("tr")
 try:
@@ -38,7 +43,8 @@ try:
 except StaleElementReferenceException:
     pass
 
-#  HumanSys work sign in
+
+#  HumanSys signin detail
 try:
     signin_button = driver.find_element_by_xpath('//*[@id="signin"]')
     signin_button.click()
--- punch_card.bat
+++ punch_card.bat
@@ -1,4 +1,3 @@
 cd /d G:/My Drive/Code/punch_card
 call activate punch_card
 call python punch.py
-REM pause
